686b0aa25e4c67112f646245c4bc321a2f7299b4
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-03 11:03:19 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,10 +1,29 @@
-const temp = 12; 
+//  const temp = 12; 
 
-if (temp <0){
-  console.log("Make sure you wear a scarf!");
-} else if (temp < 15){
-  console.log("Short sleeves wont cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+// if (temp <0){
+//   console.log("Make sure you wear a scarf!");
+// } else if (temp < 15){
+//   console.log("Short sleeves wont cut it!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+// console.log("Now you're ready to go outside!"); 
+
+// const isCitizen = true;
+// const age = 26;
+
+// if(isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// } 
+
+// const temperature = 45
+// if (temperature < -40 || temperature > 40) {
+//   console.log("Maybe going outside isn't such a great idea…");
+// }
+
+
+const raining = false
+const notRaining = !raining
+if (notRaining) {
+  console.log("Leave your umbrella at home!");
 }
-console.log("Now you're ready to go outside!");
