1fe9ca1a7e37528a700d13bd6614413855baa62c
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-03 19:11:35 -0700

    practice return greetings

--- hello.js
+++ hello.js
@@ -4,9 +4,28 @@
 
 // sayHello()
 
-const sayHello = function (name){
-  console.log("Hello, " + name);
-}
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// const sayHello = function (name){
+//   console.log("Hello, " + name);
+// }
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+// const sayHelloToConsole  = function (name) {
+//   console.log("Hello, " + name);
+// }
+// sayHelloToConsole('John'); 
+
+// const returnSayHello  = function (name) {
+//   return "Hello, " + name;
+// }
+// const greeting = returnSayHello('John');
+
+
+// const returnSayHello  = function (name) {
+//   return "Hello, " + name;
+// }
+// const greeting = returnSayHello('John');
+// console.log(greeting);
+
