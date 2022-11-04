c8c36417448523c74d9cfec6ca5fec8dc4107336
Krista Kourtessis <krista.kourtessis@telus.com>
2022-11-03 19:12:06 -0700

    first while loop

---
+++ LoopyLighthouse.js
@@ -0,0 +1,34 @@
+// // var x = 100
+
+// // while (x >= 100 && x <= 200){
+//   if (x % 3 === 0 && x % 4 === 0) {
+//     console.log("Loopylighthouse");
+//   }else if (x % 4 === 0){
+//     console.log("Lighthouse");
+//   }else(x % 3 === 0){
+//     console.log("Loopy");
+// }
+// //   x = x + 1;
+// // }
+
+
+
+// var x = 100; 
+
+// while (x <= 200){
+//   console.log(x);
+//   if (x % 3 === 0 && x % 4 === 0) {
+//     console.log("Loopylighthouse");
+//   }else if (x % 4 === 0){
+//     console.log("Lighthouse");
+//   }else if(x % 3 === 0){
+//     console.log("Loopy");
+// }
+// else console.log(x)
+//   x = x + 1
+// }
+
+var doSomething = function(y) {
+  return y + 1;
+};
+doSomething(5);
